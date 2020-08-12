c87cf3067a6973a63afc98881756b91312e4066b
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-08-12 10:18:29 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,26 +1,27 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
-from selenium.webdriver.chrome.options import Options
 import os
-
-CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
-GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
-
-
-options = Options()
-options.binary_location = GOOGLE_CHROME_BIN
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.headless = True
-
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
+import datetime
+import pandas
+import time
 
 
-url = 'https://www.amazon.es/Quimat-Pantalla-Raspberry-Protectiva-Disipadores/dp/B06W55HBTX/ref=pd_rhf_ee_s_pd_crcd_0_3/260-4071437-3846962?_encoding=UTF8&pd_rd_i=B06W55HBTX&pd_rd_r=db785bde-02bd-4dce-8abd-b6b47bd1caaa&pd_rd_w=smLre&pd_rd_wg=CSHiy&pf_rd_p=76e1f2b8-0692-47af-bb36-251bb7a6a038&pf_rd_r=NN3YXYSMHMQ6H415CEAH&psc=1&refRID=NN3YXYSMHMQ6H415CEAH'
-
-driver.get(url)
-
-el = driver.find_element_by_id('priceblock_ourprice')
-
-print(el.text)
-
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options= chrome_options)
+    
+#browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+print('browser is ready')
+url = 'https://mars.nasa.gov/news/'
+browser.get(url)
+time.sleep(3)
+html = browser.page_source
+soup = BeautifulSoup(html, 'html.parser')
+news_title = soup.find('ul', class_='item_list ').find('li', class_='slide').find('div', class_='content_title')\
+    .find('a').get_text()
+news_p = soup.find('ul', class_='item_list ').find('li', class_='slide')\
+    .find('div', class_='article_teaser_body').get_text()
+print(news_title)
